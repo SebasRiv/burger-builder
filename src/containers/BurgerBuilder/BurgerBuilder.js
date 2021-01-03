e8019e1f95cb8300a611933624e69325fd71d259
Sebastian Rivera <sebastian-010198@hotmail.com>
2021-01-02 19:37:45 -0500

    Render burger and ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Auxiliar';
 
 export class BurgerBuilder extends Component {
     render() {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger></Burger>
                 <div>Build controls</div>
             </Aux>
         )
